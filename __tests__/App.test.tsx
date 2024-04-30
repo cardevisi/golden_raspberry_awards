95d91142fa4c9f69dfbe7c3b1e66bf965d772925
Carlos Oliveira <cardevisi@gmail.com>
2024-04-30 14:48:14 -0300

    chore: setup testing library and fix some tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -5,13 +5,12 @@
 import 'react-native';
 import React from 'react';
 import App from '../src/core/app';
+import {render} from '@testing-library/react-native';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App', () => {
+  it('should renders correctly', () => {
+    render(<App />);
+  });
 });
