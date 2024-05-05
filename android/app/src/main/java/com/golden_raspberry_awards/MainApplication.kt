908f8e25e537a2ba02a6b20c77f8779845e5e7a3
Carlos Oliveira <cardevisi@gmail.com>
2024-05-04 21:05:02 -0300

    feat: remove unused import

--- android/app/src/main/java/com/golden_raspberry_awards/MainApplication.kt
+++ android/app/src/main/java/com/golden_raspberry_awards/MainApplication.kt
@@ -11,9 +11,6 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
-// react-native-vector-icons
-import com.oblador.vectoricons.VectorIconsPackage;
-
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -22,7 +19,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              add(VectorIconsPackage())
+              
             }
 
         override fun getJSMainModuleName(): String = "index"
