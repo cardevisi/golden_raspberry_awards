96a68140cb8484a3775506cd3f4e812b0673299e
Carlos Oliveira <cardevisi@gmail.com>
2024-05-04 21:59:17 -0300

    feat: adjust activity indicator on dashboard

---
+++ src/components/dashboard/dashboard.style.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  keyboardAvoidingContainer: {
+    flex: 1,
+  },
+  scrollviewContainer: {
+    flexGrow: 1,
+  },
+});
+export default styles;
--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -12,6 +12,7 @@ import {
   Alert,
   KeyboardAvoidingView,
   ScrollView,
+  View,
 } from 'react-native';
 import {
   useMultipleWinnersByYears,
@@ -23,6 +24,7 @@ import {
 import {Box} from '@golden-raspberry-awards/shared/box';
 import {SearchBar, Title} from '@golden-raspberry-awards/shared';
 import {ThemeProps} from '../../theme';
+import styles from './dashboard.style';
 
 const Text = createText<ThemeProps>();
 
@@ -65,117 +67,119 @@ const DashboardBase = ({name}: DashboardProps) => {
   }, [refetchWinnersByYear, selectedWinnersByYear]);
 
   return (
-    <KeyboardAvoidingView behavior="position">
-      <ScrollView>
-        <Box bg="black" flex={1} paddingHorizontal="s">
-          <Box
-            flexDirection="row"
-            justifyContent="space-between"
-            backgroundColor="black"
-            alignContent="center"
-            alignItems="center"
-            paddingHorizontal="s">
-            <Title name={name} />
-            {isLoadingWinnersByYear &&
-            isLoadingMinMaxWins &&
-            isLoadingMultiplesWinnersByYear &&
-            isLoadingStudiosWithWinners ? (
+    <KeyboardAvoidingView
+      behavior="padding"
+      style={styles.keyboardAvoidingContainer}
+      enabled>
+      <ScrollView contentContainerStyle={styles.scrollviewContainer}>
+        <Box paddingHorizontal="s">
+          <Title name={name} />
+        </Box>
+        <Box paddingHorizontal="s" flex={1} justifyContent="center">
+          {isLoadingWinnersByYear &&
+          isLoadingMinMaxWins &&
+          isLoadingMultiplesWinnersByYear &&
+          isLoadingStudiosWithWinners ? (
+            <Box>
               <ActivityIndicator size="small" aria-label="activity-indicator" />
-            ) : null}
-          </Box>
-          <Box marginBottom="l" paddingHorizontal="s">
-            <Box flexDirection="column" marginBottom="s" borderRadius={5}>
-              <Text variant="body" color="white" fontWeight={'bold'}>
-                List years with multiple winners
-              </Text>
             </Box>
-            <TableListMultipleWinnersBase
-              isLoading={false}
-              data={queryResultMultiplesWinnersByYear?.years}
-              onPress={(item: any) => {
-                Alert.alert(
-                  item.year.toString(),
-                  `Number of winners: ${item.winnerCount.toString()}`,
-                );
-              }}
-            />
-          </Box>
-          <Box marginBottom="l" paddingHorizontal="s">
-            <Box flexDirection="column" marginBottom="s" borderRadius={5}>
-              <Text variant="body" color="white" fontWeight={'bold'}>
-                {`Top ${TOP_WINNERS} studios with winners`}
-              </Text>
-            </Box>
-            <TableListTopWinnersBase
-              isLoading={false}
-              data={queryResultStudiosWithWinners}
-              onPress={(item: any) => {
-                Alert.alert(
-                  item.name,
-                  `Win count: ${item.winCount.toString()}`,
-                );
-              }}
-            />
-          </Box>
-          <Box marginBottom="l" paddingHorizontal="s">
-            <Box flexDirection="column" marginBottom="s" borderRadius={5}>
-              <Text variant="body" color="white" fontWeight={'bold'}>
-                Producers with longest and
-              </Text>
-              <Text variant="body" color="white" fontWeight={'bold'}>
-                shortest intervals
-              </Text>
-            </Box>
-            <TableListMinMaxWinnersBase
-              isLoading={false}
-              label={'Maximum'}
-              data={queryResultMaxWins}
-              onPress={(item: any) => {
-                Alert.alert(
-                  item.producer,
-                  `Interval: ${item.interval.toString()}`,
-                );
-              }}
-            />
-            <Box marginTop="s">
-              <TableListMinMaxWinnersBase
-                isLoading={false}
-                label={'Minimum'}
-                data={queryResultMinWins}
-                onPress={(item: any) => {
-                  Alert.alert(
-                    item.producer,
-                    `Interval: ${item.interval.toString()}`,
-                  );
-                }}
-              />
-            </Box>
-            <Box marginTop="l">
-              <Box flexDirection="column" marginBottom="s" borderRadius={5}>
-                <Text variant="body" color="white" fontWeight={'bold'}>
-                  List movie winners by year
-                </Text>
+          ) : (
+            <>
+              <Box marginBottom="l" paddingHorizontal="s">
+                <Box flexDirection="column" marginBottom="s" borderRadius={5}>
+                  <Text variant="body" color="white" fontWeight={'bold'}>
+                    List years with multiple winners
+                  </Text>
+                </Box>
+                <TableListMultipleWinnersBase
+                  isLoading={false}
+                  data={queryResultMultiplesWinnersByYear?.years}
+                  onPress={(item: any) => {
+                    Alert.alert(
+                      item.year.toString(),
+                      `Number of winners: ${item.winnerCount.toString()}`,
+                    );
+                  }}
+                />
               </Box>
-              <SearchBar
-                searchPhrase={selectedWinnersByYear.toString()}
-                setSearchPhrase={yearValue => {
-                  setSelectedWinnersByYear(yearValue);
-                }}
-                maxLength={4}
-                inputMode="numeric"
-              />
-              <TableListWinnersByYearBase
-                isLoading={false}
-                data={queryResultWinnersByYear}
-                onPress={(item: any) => {
-                  Alert.alert(
-                    item.title,
-                    `Producers: ${item.producers.join(', ')}`,
-                  );
-                }}
-              />
-            </Box>
-          </Box>
+              <Box marginBottom="l" paddingHorizontal="s">
+                <Box flexDirection="column" marginBottom="s" borderRadius={5}>
+                  <Text variant="body" color="white" fontWeight={'bold'}>
+                    {`Top ${TOP_WINNERS} studios with winners`}
+                  </Text>
+                </Box>
+                <TableListTopWinnersBase
+                  isLoading={false}
+                  data={queryResultStudiosWithWinners}
+                  onPress={(item: any) => {
+                    Alert.alert(
+                      item.name,
+                      `Win count: ${item.winCount.toString()}`,
+                    );
+                  }}
+                />
+              </Box>
+              <Box marginBottom="l" paddingHorizontal="s">
+                <Box flexDirection="column" marginBottom="s" borderRadius={5}>
+                  <Text variant="body" color="white" fontWeight={'bold'}>
+                    Producers with longest and
+                  </Text>
+                  <Text variant="body" color="white" fontWeight={'bold'}>
+                    shortest intervals
+                  </Text>
+                </Box>
+                <TableListMinMaxWinnersBase
+                  isLoading={false}
+                  label={'Maximum'}
+                  data={queryResultMaxWins}
+                  onPress={(item: any) => {
+                    Alert.alert(
+                      item.producer,
+                      `Interval: ${item.interval.toString()}`,
+                    );
+                  }}
+                />
+                <Box marginTop="s">
+                  <TableListMinMaxWinnersBase
+                    isLoading={false}
+                    label={'Minimum'}
+                    data={queryResultMinWins}
+                    onPress={(item: any) => {
+                      Alert.alert(
+                        item.producer,
+                        `Interval: ${item.interval.toString()}`,
+                      );
+                    }}
+                  />
+                </Box>
+                <Box marginTop="l">
+                  <Box flexDirection="column" marginBottom="s" borderRadius={5}>
+                    <Text variant="body" color="white" fontWeight={'bold'}>
+                      List movie winners by year
+                    </Text>
+                  </Box>
+                  <SearchBar
+                    searchPhrase={selectedWinnersByYear.toString()}
+                    setSearchPhrase={yearValue => {
+                      setSelectedWinnersByYear(yearValue);
+                    }}
+                    maxLength={4}
+                    inputMode="numeric"
+                  />
+                  <TableListWinnersByYearBase
+                    isLoading={false}
+                    data={queryResultWinnersByYear}
+                    onPress={(item: any) => {
+                      Alert.alert(
+                        item.title,
+                        `Producers: ${item.producers.join(', ')}`,
+                      );
+                    }}
+                  />
+                </Box>
+              </Box>
+            </>
+          )}
         </Box>
       </ScrollView>
     </KeyboardAvoidingView>
